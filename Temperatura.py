d51ac51e37cc1a2e99396bf43bdf1ce6398d4e1b
Kaelith-glith <edilsoncraft0@gmail.com>
2025-05-08 15:16:38 -0300

    Primeiro commit

---
+++ Temperatura.py
@@ -0,0 +1,11 @@
+temp = int(input('Qual a temperatura hoje? '))
+
+if temp > 25:
+    print('Hoje está quente!')
+
+elif temp < 18:
+    print('Hoje esta frio!')
+
+else: 
+    print('Hoje está agradavel!')
+
