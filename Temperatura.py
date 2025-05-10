19ec49a7fa4fc8ebc264afd707c99edd8414eb90
Kaelith-glith <edilsoncraft0@gmail.com>
2025-05-10 11:56:25 -0300

    deixei o codigo mais robusto e com restrinção de idade

--- Temperatura.py
+++ Temperatura.py
@@ -1,11 +1,34 @@
-temp = int(input('Qual a temperatura hoje? '))
+nome = input('Qual seu nome? ')
+idade = int(input('Sua idade? '))
 
-if temp > 25:
-    print('Hoje está quente!')
+if idade < 18:
+    print(f'Desculpe, {nome}, mas apenas maiores de idade podem acessar o sistema.')
+else:
+    print(f'Bem-vindo ao sistema, {nome}!')
 
-elif temp < 18:
-    print('Hoje esta frio!')
+    # Parte com loop para temperatura
+    def escolhas():
+        while True:
+            print('\nDigite 1 para verificar temperatura ou 2 para sair.')
+            escolha = input('Escolha: ')
 
-else: 
-    print('Hoje está agradavel!')
+            if escolha == '2':
+                print('Saindo do sistema... Até mais!')
+                break
 
+            elif escolha == '1':
+                temp = int(input('Qual a temperatura hoje? '))
+
+                if temp > 25:
+                    print(f'Hoje está quente, {nome}!')
+                elif temp < 0:
+                    print('Hoje está muito frio, véi!')
+                elif temp <= 24:
+                    print('Hoje está frio!')
+                else:
+                    print('Hoje está agradável!')
+            else:
+                print('Opção inválida. Tente novamente.')
+
+    # Só chama a função se a idade for válida
+    escolhas()
